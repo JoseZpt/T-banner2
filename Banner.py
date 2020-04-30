c94cf692700883e11c34de0d15e3e0bb9402353b
Anonymous_Zpt <63219390+Anonymous-Zpt@users.noreply.github.com>
2020-04-30 17:09:41 -0500

    Create Banner.py

---
+++ Banner.py
@@ -0,0 +1,244 @@
+#SIGUEME EM YOUTUBE
+
+#coding=utf-8
+
+import os,sys,time
+
+def maung(n):
+
+    for m in n + '\n':
+
+        sys.stdout.write(m)
+
+        sys.stdout.flush()
+
+        time.sleep(0.001)
+
+hehe = """\033[94m
+
+              __      _
+
+              \.'---.//|
+
+         \033[95m•\033[90m─────\033[94m|\./|  \/\033[90m────────────────────────\033[95m•
+
+          \033[95m•\033[90m───\033[94m_|\033[95m●\033[94m|\033[95m●\033[94m|_  \ \033[90m──────────────────────\033[95m•
+
+           \033[95m•\033[90m─\033[94m/(  ) ```  \  \033[91m╔≓═╗╥╭═╗ ╔╔══╗\033[90m─────\033[95m•
+
+           \033[95m•\033[94m|  \/   . |  \ \033[93m╠══╣║║ ║ ║║  ╤\033[90m─────\033[95m•
+
+           \033[95m•\033[90m─\033[94m\_/\__/| |   \033[92m ╿  ╜╽╝ ╚≒╝╚═≒╝\033[90m─────\033[95m•
+
+           \033[95m•\033[90m──\033[97mV  \033[94m/\033[97mV\033[94m / | \033[92m╔═╦═╗╔≑═╗╽  ╥╔═╗ ╔╔═≓╡\033[90m\033[95m•
+
+           \033[95m•\033[90m────\033[94m/__/ /  \033[93m╫ ╽ ║╠══╣║  ║║ ╫ ║╫  ╕\033[90m\033[95m•
+
+           \033[95m•\033[90m────\033[94m\___/\__\033[91m╜\033[90m_\033[91m  ╨╿  ╩╚═≓╝╽ ╚═╯╚══╜\033[90m\033[95m•
+
+           \033[95m•\033[90m─────────\033[97m[\033[92m  -Termux-Banner2-   \033[97m]\033[90m───\033[95m•
+
+          \033[95m•\033[90m────────────────────────────────────\033[95m•"""
+
+os.system("clear")
+
+maung(hehe)
+
+os.system("clear")
+
+print (hehe)
+
+os.system("clear")
+
+print (hehe)
+
+os.system("clear")
+
+print (hehe)
+
+os.system("clear")
+
+print (hehe)
+
+os.system("clear")
+
+print (hehe)
+
+os.system("clear")
+
+print (hehe)
+
+os.system("clear")
+
+print (hehe)
+
+os.system("clear")
+
+print (hehe)
+
+os.system("clear")
+
+print (hehe)
+
+os.system("clear")
+
+print (hehe)
+
+os.system("clear")
+
+print (hehe)
+
+os.system("clear")
+
+print (hehe)
+
+os.system("clear")
+
+print (hehe)
+
+os.system("clear")
+
+print (hehe)
+
+os.system("clear")
+
+print (hehe)
+
+os.system("clear")
+
+print (hehe)
+
+os.system("clear")
+
+print (hehe)
+
+os.system("clear")
+
+print (hehe)
+
+os.system("clear")
+
+print (hehe)
+
+os.system("clear")
+
+print (hehe)
+
+os.system("clear")
+
+print (hehe)
+
+os.system("clear")
+
+print (hehe)
+
+os.system("clear")
+
+print (hehe)
+
+os.system("clear")
+
+print (hehe)
+
+os.system("clear")
+
+print (hehe)
+
+os.system("clear")
+
+print (hehe)
+
+os.system("clear")
+
+print (hehe)
+
+os.system("clear")
+
+print (hehe)
+
+os.system("clear")
+
+print (hehe)
+
+os.system("clear")
+
+print (hehe)
+
+os.system("clear")
+
+print (hehe)
+
+os.system("clear")
+
+print (hehe)
+
+os.system("clear")
+
+print (hehe)
+
+os.system("clear")
+
+print (hehe)
+
+os.system("clear")
+
+print (hehe)
+
+os.system("clear")
+
+print (hehe)
+
+os.system("clear")
+
+print (hehe)
+
+os.system("clear")
+
+print (hehe)
+
+os.system("clear")
+
+print (hehe)
+
+os.system("clear")
+
+print (hehe)
+
+os.system("clear")
+
+print (hehe)
+
+os.system("clear")
+
+print (hehe)
+
+os.system("clear")
+
+print (hehe)
+
+os.system("clear")
+
+print (hehe)
+
+os.system("clear")
+
+print (hehe)
+
+os.system("clear")
+
+print (hehe)
+
+os.system("clear")
+
+print (hehe)
+
+os.system("clear")
+
+print (hehe)
+
+os.system("clear")
+
+print (hehe)
+
+os.system("      sh FECHA.sh")
+
